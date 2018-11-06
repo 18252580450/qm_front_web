161622324ff0236fa2ad25f7a958e0e652563f8f
dingzc <dingzc@asiainfo.com>
2018-11-06 18:30:09 +0800

    考评项新增

--- src/main/resources/static/qm/js/manage/checkItemManage.js
+++ src/main/resources/static/qm/js/manage/checkItemManage.js
@@ -1,48 +1,70 @@
 require(["jquery", 'util', "transfer", "easyui"], function ($, Util, Transfer) {
     var qmURI = "/qm/configservice/checkItem";
-    //调用初始化方法
+    //初始化方法
     initialize();
 
     function initialize() {
-        initGrid();
-        initGlobalEvent();
+        initPageInfo();
+        initEvent();
         //initWindowEvent();
         //initReviseEvent();
     };
 
-    //格式化时间方法
-    function formatDateTime(inputTime) {
-        var date = new Date(inputTime);
-        var y = date.getFullYear();
-        var m = date.getMonth() + 1;
-        m = m < 10 ? ('0' + m) : m;
-        var d = date.getDate();
-        d = d < 10 ? ('0' + d) : d;
-        var h = date.getHours();
-        h = h < 10 ? ('0' + h) : h;
-        var minute = date.getMinutes();
-        var second = date.getSeconds();
-        minute = minute < 10 ? ('0' + minute) : minute;
-        second = second < 10 ? ('0' + second) : second;
-        return y + '-' + m + '-' + d + ' ' + h + ':' + minute + ':' + second;
-    }
+    //页面信息初始化
+    function initPageInfo() {
+        //模板渠道下拉框
+        $("#tenantType").combobox({
+            url: '../../data/tenant_type.json',
+            method: "GET",
+            valueField: 'codeValue',
+            textField: 'codeName',
+            panelHeight:'auto',
+            editable:false,
+            onLoadSuccess : function(){
+                var tenantType = $("#tenantType");
+                var data = tenantType.combobox('getData');
+                if (data.length > 0) {
+                    tenantType.combobox('select', data[0].codeValue);
+                }
+            },
+            onSelect: function () {
+                $("#checkItemList").datagrid("load");
+            }
+        });
+        //考评项类型下拉框
+        $("#checkItemType").combobox({
+            url: '../../data/check_item_type.json',
+            method: "GET",
+            valueField: 'codeValue',
+            textField: 'codeName',
+            panelHeight:'auto',
+            editable:false,
+            onLoadSuccess : function(){
+                var checkItemType = $('#checkItemType');
+                var data = checkItemType.combobox('getData');
+                if (data.length > 0) {
+                    checkItemType.combobox('select', data[0].codeValue);
+                }
+            },
+            onSelect: function () {
+                $("#checkItemList").datagrid("load");
+            }
+        });
 
-    //初始化列表
-    function initGrid() {
+        //考评项列表
         var IsCheckFlag = true; //标示是否是勾选复选框选中行的，true - 是 , false - 否
         $("#checkItemList").datagrid({
             columns: [[
                 {field: 'checkitemId', title: '考评项ID', hidden: true},
                 {field: 'ck', checkbox: true, align: 'center'},
-                {field: 'checkitemName', title: '考评项名称', align: 'center', width: '25%'},
-                {field: 'checkitemType', title: '考评项类别', align: 'center', width: '25%'},
-                {field: 'remark', title: '考评项描述', align: 'center', width: '35%'},
+                {field: 'checkitemName', title: '考评项名称', align: 'center', width: '20%'},
+                {field: 'checkitemType', title: '考评项类型', align: 'center', width: '20%'},
+                {field: 'checkitemVitalType', title: '致命类别', align: 'center', width: '13%'},
+                {field: 'remark', title: '考评项描述', align: 'center', width: '30%'},
                 {
-                    field: 'action', title: '操作', align: 'center', width: '10%',
+                    field: 'action', title: '操作', align: 'center', width: '13%',
                     formatter: function (value, row, index) {
-                        var Action =
-                            "<a href='javascript:void(0);' class='reviseBtn' id = 'checkItem'" + row.checkItemId + " >修改</a>";
-                        return Action;
+                            return "<a href='javascript:void(0);' class='reviseBtn' id = 'checkItem'" + row.checkItemId + " >修改</a>";
                     }
                 }
             ]],
@@ -76,12 +98,15 @@ require(["jquery", 'util', "transfer", "easyui"], function ($, Util, Transfer) {
                 var start = (param.page - 1) * param.rows;
                 var pageNum = param.rows;
                 var checkItemName = $("#checkItemName").val();
-                var tenantId = $("#tenantName").val();
-                var checkItemType = $("#checkItemType").val();
+                var tenantId = $("#tenantType").combobox("getValue");
+                var checkItemType = $("#checkItemType").combobox("getValue");
+                if(checkItemType === "4"){
+                    checkItemType = null;
+                }
                 var reqParams = {
                     "parentCheckItemId":Util.constants.PARENT_CHECK_ITEM_ID,
                     "checkItemName":checkItemName,
-                    "tenantId":Util.constants.TENANT_ID,
+                    "tenantId":tenantId,
                     "checkItemType": checkItemType
                 };
                 var params = $.extend({
@@ -91,11 +116,10 @@ require(["jquery", 'util', "transfer", "easyui"], function ($, Util, Transfer) {
                 }, Util.PageUtil.getParams($("#searchForm")));
 
                 Util.ajax.getJson(Util.constants.CONTEXT + qmURI + "/queryCheckItem", params, function (result) {
-                    debugger;
                     var data = Transfer.DataGrid.transfer(result);
 
                     var rspCode = result.RSP.RSP_CODE;
-                    if (rspCode != "1") {
+                    if (rspCode != null && rspCode !== "1") {
                         $.messager.show({
                             msg: result.RSP.RSP_DESC,
                             timeout: 1000,
@@ -109,8 +133,8 @@ require(["jquery", 'util', "transfer", "easyui"], function ($, Util, Transfer) {
         });
     }
 
-    //初始化事件
-    function initGlobalEvent() {
+    //事件初始化
+    function initEvent() {
         //查询
         $("#queryBtn").on("click", function () {
             $("#checkItemList").datagrid("load");
@@ -118,40 +142,193 @@ require(["jquery", 'util', "transfer", "easyui"], function ($, Util, Transfer) {
 
         //新增
         $("#addBtn").on("click", function () {
-
+            showCheckItemCreateDialog();
         });
+
+        //修改
+
 
         //删除
         $("#delBtn").on("click", function () {
-            var delRows = $("#checkItemList").datagrid("getSelections");
-            if (delRows.length == 0) {
-                $.messager.alert("提示", "请至少选择一行数据!");
+            showCheckItemDeleteDialog();
+        });
+    }
+
+    /**
+     * 新增考评项弹框
+     */
+    function showCheckItemCreateDialog() {
+        $("#checkItemConfig").form('clear');  //清空表单
+        $("#checkItemDialog").show().window({
+            width: 600,
+            height: 350,
+            modal: true,
+            title: "新增考评项"
+        });
+        //考评项类型下拉框
+        $("#checkItemTypeConfig").combobox({
+            url: '../../data/check_item_config_type.json',
+            method: "GET",
+            valueField: 'codeValue',
+            textField: 'codeName',
+            panelHeight:'auto',
+            editable:false,
+            onLoadSuccess : function(){
+                var checkItemType = $('#checkItemTypeConfig');
+                var data = checkItemType.combobox('getData');
+                if (data.length > 0) {
+                    checkItemType.combobox('select', data[0].codeValue);
+                }
+            }
+        });
+        //考评项致命类别下拉框
+        $("#checkItemVitalTypeConfig").combobox({
+            url: '../../data/check_item_vital_type.json',
+            method: "GET",
+            valueField: 'codeValue',
+            textField: 'codeName',
+            panelHeight:'auto',
+            editable:false,
+            onLoadSuccess : function(){
+                var checkItemVitalType = $('#checkItemVitalTypeConfig');
+                var data = checkItemVitalType.combobox('getData');
+                if (data.length > 0) {
+                    checkItemVitalType.combobox('select', data[0].codeValue);
+                }
+            }
+        });
+        //取消
+        var cancelBtn = $("#cancelBtn");
+        cancelBtn.unbind("click");
+        cancelBtn.on("click",function () {
+            $("#checkItemConfig").form('clear');  //清空表单
+            $("#checkItemDialog").window("close");
+        });
+        //提交
+        var submitBtn = $("#submitBtn");
+        submitBtn.unbind("click");
+        submitBtn.on("click",function () {
+            submitBtn.linkbutton({disabled: true});  //防止多次提交
+
+            var checkItemName = $("#checkItemNameConfig").val();
+            var checkItemType = $("#checkItemTypeConfig").combobox("getValue");
+            var checkItemVitalType = $("#checkItemVitalTypeConfig").combobox("getValue");
+            var checkItemDesc = $("#checkItemDescConfig").val();
+
+            if(checkItemName == null || checkItemName ===""){
+                $.messager.alert("提示", "考评项名称不能为空!");
                 return false;
             }
-            var delArr = [];
-            for (var i = 0; i < delRows.length; i++) {
-                var id = delRows[i].checkitemId;
-                delArr.push(id);
-            }
-            $.messager.confirm('确认删除弹窗', '确定要删除吗？', function (confirm) {
-                if (confirm) {
-                    Util.ajax.deleteJson(Util.constants.CONTEXT.concat(qmURI).concat("/deleteCheckItem/").concat(delArr), {}, function (result) {
-
-                        $.messager.show({
-                            msg: result.RSP.RSP_DESC,
-                            timeout: 1000,
-                            style: {right: '', bottom: ''},     //居中显示
-                            showType: 'slide'
-                        });
-
-                        var rspCode = result.RSP.RSP_CODE;
-
-                        if (rspCode == "1") {
-                            $("#checkItemList").datagrid('reload'); //删除成功后，刷新页面
-                        }
-                    });
+            var params = {
+                "tenantId":Util.constants.TENANT_ID,
+                "parentCheckitemId":Util.constants.PARENT_CHECK_ITEM_ID,
+                'checkitemName':checkItemName,
+                'checkitemType':checkItemType,
+                'checkitemVitalType':checkItemVitalType,
+                'remark':checkItemDesc
+            };
+            Util.ajax.postJson(Util.constants.CONTEXT.concat(qmURI).concat("/"), JSON.stringify(params), function (result) {
+                $.messager.show({
+                    msg: result.RSP.RSP_DESC,
+                    timeout: 1000,
+                    style: {right: '', bottom: ''},     //居中显示
+                    showType: 'slide'
+                });
+                var rspCode = result.RSP.RSP_CODE;
+                if (rspCode != null && rspCode === "1") {
+                    $("#checkItemList").datagrid('reload'); //插入成功后，刷新页面
                 }
             });
+        });
+    }
+
+    /**
+     * 修改考评项弹框
+     */
+    function showCheckItemUpdateDialog() {
+        $("#checkItemConfig").form('clear');  //清空表单
+        $("#checkItemDialog").show().window({
+            width: 600,
+            height: 350,
+            modal: true,
+            title: "修改考评项"
+        });
+        //考评项类型下拉框
+        $("#checkItemTypeConfig").combobox({
+            url: '../../data/check_item_config_type.json',
+            method: "GET",
+            valueField: 'codeValue',
+            textField: 'codeName',
+            panelHeight:'auto',
+            editable:false,
+            onLoadSuccess : function(){
+                var checkItemType = $('#checkItemTypeConfig');
+                var data = checkItemType.combobox('getData');
+                if (data.length > 0) {
+                    checkItemType.combobox('select', data[0].codeValue);
+                }
+            }
+        });
+        //考评项致命类别下拉框
+        $("#checkItemVitalTypeConfig").combobox({
+            url: '../../data/check_item_vital_type.json',
+            method: "GET",
+            valueField: 'codeValue',
+            textField: 'codeName',
+            panelHeight:'auto',
+            editable:false,
+            onLoadSuccess : function(){
+                var checkItemVitalType = $('#checkItemVitalTypeConfig');
+                var data = checkItemVitalType.combobox('getData');
+                if (data.length > 0) {
+                    checkItemVitalType.combobox('select', data[0].codeValue);
+                }
+            }
+        });
+        //取消
+        var cancelBtn = $("#cancelBtn");
+        cancelBtn.unbind("click");
+        cancelBtn.on("click",function () {
+            $("#checkItemConfig").form('clear');  //清空表单
+            $("#checkItemDialog").window("close");
+        });
+        //提交
+        var submitBtn = $("#submitBtn");
+        submitBtn.unbind("click");
+        submitBtn.on("click",function () {
+            submitBtn.linkbutton({disabled: true});  //防止多次提交
+        });
+    }
+
+    /**
+     * 删除考评项弹框
+     */
+    function showCheckItemDeleteDialog() {
+        var delRows = $("#checkItemList").datagrid("getSelections");
+        if (delRows.length === 0) {
+            $.messager.alert("提示", "请至少选择一行数据!");
+            return false;
+        }
+        var delArr = [];
+        for (var i = 0; i < delRows.length; i++) {
+            var id = delRows[i].checkitemId;
+            delArr.push(id);
+        }
+        $.messager.confirm('确认删除弹窗', '确定要删除吗？', function (confirm) {
+            if (confirm) {
+                Util.ajax.deleteJson(Util.constants.CONTEXT.concat(qmURI).concat("/").concat(delArr), {}, function (result) {
+                    $.messager.show({
+                        msg: result.RSP.RSP_DESC,
+                        timeout: 1000,
+                        style: {right: '', bottom: ''},     //居中显示
+                        showType: 'slide'
+                    });
+                    var rspCode = result.RSP.RSP_CODE;
+                    if (rspCode != null && rspCode === "1") {
+                        $("#checkItemList").datagrid('reload'); //删除成功后，刷新页面
+                    }
+                });
+            }
         });
     }
 
