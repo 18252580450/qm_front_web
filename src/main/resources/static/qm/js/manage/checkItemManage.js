fc61c856487c9558f4a3ba2d2d1f79a88f0b95e1
dingzc <dingzc@asiainfo.com>
2018-12-11 09:37:02 +0800

    新增考评树

--- src/main/resources/static/qm/js/manage/checkItemManage.js
+++ src/main/resources/static/qm/js/manage/checkItemManage.js
@@ -1,8 +1,17 @@
-require(["jquery", 'util', "transfer", "easyui"], function ($, Util, Transfer) {
+require(["jquery", 'util', "transfer", "easyui", "ztree-exedit"], function ($, Util, Transfer) {
 
-    var checkTypeData = [],    //考评项下拉框静态数据
+    var setting,               //ztree配置
+        checkTypeData = [],    //考评项下拉框静态数据
+        checkItemListData = [],//所有考评项
         checkItemData = [],    //新增考评项下拉框静态数据
-        vitalTypeData = [];    //新增致命类别下拉框静态数据
+        vitalTypeData = [],    //新增致命类别下拉框静态数据
+        checkNode = {          //左侧考评树选中节点
+            id: "",
+            parentId: "",
+            name: "",
+            level: "",
+            isParent: ""
+        };
 
     initialize();
 
@@ -29,7 +38,7 @@ require(["jquery", 'util', "transfer", "easyui"], function ($, Util, Transfer) {
                 }
             },
             onSelect: function () {
-                $("#checkItemList").datagrid("load");
+                $("#checkItemList").datagrid('reload');
             }
         });
         //考评项类型下拉框
@@ -48,11 +57,68 @@ require(["jquery", 'util', "transfer", "easyui"], function ($, Util, Transfer) {
                 }
             },
             onSelect: function () {
-                $("#checkItemList").datagrid("load");
+                $("#checkItemList").datagrid('reload');
             }
         });
         //重载下拉框数据
-        reloadSelectData("CHECK_ITEM_TYPE", "checkItemType", true)
+        reloadSelectData("CHECK_ITEM_TYPE", "checkItemType", true);
+
+        //初始化考评树
+        setting = {
+            view: {
+                selectedMulti: false//是否支持同时选中多个节点
+            },
+            data: {
+                key: {
+                    //将treeNode的checkItemName属性当做节点名称
+                    name: "name"
+                },
+                simpleData: {
+                    enable: true,   //是否异步
+                    idKey: "id",    //当前节点id属性
+                    pIdKey: "pId",  //当前节点的父节点id属性
+                    rootPId: 0      //用于修正根节点父节点数据，即pIdKey指定的属性值
+                }
+            },
+            callback: {
+                onClick: function (e, id, node) {   //点击事件
+                    checkNode.id = node.id;
+                    checkNode.parentId = node.pId;
+                    checkNode.name = node.name;
+                    checkNode.level = node.level;
+                    checkNode.isParent = node.isParent;
+                    if (node.isParent) {
+                        $("#parentCheckItemName").val(node.name);
+                        $("#checkItemList").datagrid('reload');
+                    } else {
+                        var data = [];
+                        $.each(checkItemListData, function (i, item) {
+                            if (node.id === item.checkItemId) {
+                                data.push(item);
+                            }
+                            if (node.pId === item.checkItemId) {
+                                $("#parentCheckItemName").val(item.checkItemName);
+                            }
+                        });
+                        $("#checkItemList").datagrid('loadData', data);
+                    }
+
+                    // if (node.isParent) {//父节点
+                    //     $("#page").off("click", "#addTemplate");//解决点击多次ztree之后再点击按钮后，被多次调用
+                    //     $("#page").on("click", "#addTemplate", function () {//点击父节点,然后跳出弹出框，右侧新增
+                    //         addWindowEvent(node, true);
+                    //     });
+                    //
+                    // } else {//点击子节点，右侧直接新增
+                    //     $("#page").off("click", "#addTemplate");
+                    //     $("#page").on("click", "#addTemplate", function () {//点击子节点,然后右侧新增
+                    //         addWindowEvent(node, false);
+                    //     });
+                    // }
+                }
+            }
+        };
+        refreshTree();
 
         //考评项列表
         var IsCheckFlag = true; //标示是否是勾选复选框选中行的，true - 是 , false - 否
@@ -121,16 +187,23 @@ require(["jquery", 'util', "transfer", "easyui"], function ($, Util, Transfer) {
                 }
             },
             loader: function (param, success) {
+                var parentCheckItemId = checkNode.id;
                 var start = (param.page - 1) * param.rows;
                 var pageNum = param.rows;
                 var checkItemName = $("#checkItemName").val();
                 var tenantId = $("#tenantType").combobox("getValue");
+                if (tenantId === "") {
+                    tenantId = Util.constants.TENANT_ID;
+                }
+                if (parentCheckItemId === undefined || parentCheckItemId == null) {
+                    parentCheckItemId = "";
+                }
                 var checkItemType = $("#checkItemType").combobox("getValue");
                 if (checkItemType === "-1") {
-                    checkItemType = null;
+                    checkItemType = "";
                 }
                 var reqParams = {
-                    "parentCheckItemId": Util.constants.PARENT_CHECK_ITEM_ID,
+                    "parentCheckItemId": parentCheckItemId,
                     "checkItemName": checkItemName,
                     "tenantId": tenantId,
                     "checkItemType": checkItemType
@@ -142,8 +215,6 @@ require(["jquery", 'util', "transfer", "easyui"], function ($, Util, Transfer) {
                 }, Util.PageUtil.getParams($("#searchForm")));
 
                 Util.ajax.getJson(Util.constants.CONTEXT + Util.constants.CHECK_ITEM_DNS + "/queryCheckItem", params, function (result) {
-                    var data = Transfer.DataGrid.transfer(result);
-
                     var rspCode = result.RSP.RSP_CODE;
                     if (rspCode != null && rspCode !== "1") {
                         $.messager.show({
@@ -152,6 +223,8 @@ require(["jquery", 'util', "transfer", "easyui"], function ($, Util, Transfer) {
                             style: {right: '', bottom: ''},     //居中显示
                             showType: 'show'
                         });
+                    } else {
+                        var data = Transfer.DataGrid.transfer(result);
                     }
                     success(data);
                 });
@@ -171,7 +244,17 @@ require(["jquery", 'util', "transfer", "easyui"], function ($, Util, Transfer) {
     function initEvent() {
         //查询
         $("#queryBtn").on("click", function () {
-            $("#checkItemList").datagrid("load");
+            if (node.isParent) {
+                $("#checkItemList").datagrid('reload');
+            } else {
+                var data = [];
+                $.each(checkItemListData, function (i, item) {
+                    if (node.id === item.checkItemId) {
+                        data.push(item);
+                    }
+                });
+                $("#checkItemList").datagrid('loadData', data);
+            }
         });
 
         //新增
@@ -189,6 +272,10 @@ require(["jquery", 'util', "transfer", "easyui"], function ($, Util, Transfer) {
      * 新增考评项弹框
      */
     function showCheckItemCreateDialog() {
+        if (!checkNode.isParent) {
+            $.messager.alert("提示", "请选择目录!");
+            return false;
+        }
         $("#checkItemConfig").form('clear');  //清空表单
         var disableSubmit = false;  //禁用提交按钮标志
         $("#checkItemDialog").show().window({
@@ -256,11 +343,11 @@ require(["jquery", 'util', "transfer", "easyui"], function ($, Util, Transfer) {
             disableSubmit = true;   //防止多次提交
             submitBtn.linkbutton({disabled: true});  //禁用提交按钮（样式）
 
-            var tenantId = $("#tenantType").combobox("getValue");
-            var checkItemName = $("#checkItemNameConfig").val();
-            var checkItemType = $("#checkItemTypeConfig").combobox("getValue");
-            var checkItemVitalType = $("#checkItemVitalTypeConfig").combobox("getValue");
-            var checkItemDesc = $("#checkItemDescConfig").val();
+            var tenantId = $("#tenantType").combobox("getValue"),
+                checkItemName = $("#checkItemNameConfig").val(),
+                checkItemType = $("#checkItemTypeConfig").combobox("getValue"),
+                checkItemVitalType = $("#checkItemVitalTypeConfig").combobox("getValue"),
+                checkItemDesc = $("#checkItemDescConfig").val();
 
             if (checkItemName == null || checkItemName === "") {
                 $.messager.alert("提示", "考评项名称不能为空!");
@@ -270,11 +357,13 @@ require(["jquery", 'util', "transfer", "easyui"], function ($, Util, Transfer) {
             }
             var params = {
                 "tenantId": tenantId,
-                "parentCheckItemId": Util.constants.PARENT_CHECK_ITEM_ID,
+                "parentCheckItemId": checkNode.id,
                 "checkItemName": checkItemName,
                 "checkItemType": checkItemType,
                 "checkItemVitalType": checkItemVitalType,
-                "remark": checkItemDesc
+                "remark": checkItemDesc,
+                "catalogFlag": 1,
+                "orderNo": checkNode.level + 1
             };
             Util.ajax.postJson(Util.constants.CONTEXT.concat(Util.constants.CHECK_ITEM_DNS).concat("/"), JSON.stringify(params), function (result) {
                 $.messager.show({
@@ -287,6 +376,7 @@ require(["jquery", 'util', "transfer", "easyui"], function ($, Util, Transfer) {
                 if (rspCode != null && rspCode === "1") {
                     $("#checkItemDialog").window("close");  //关闭对话框
                     $("#checkItemList").datagrid('reload'); //插入成功后，刷新页面
+                    refreshTree(); //刷新考评树
                 }
                 disableSubmit = false;
                 submitBtn.linkbutton({disabled: false});  //取消提交禁用
@@ -320,7 +410,7 @@ require(["jquery", 'util', "transfer", "easyui"], function ($, Util, Transfer) {
             editable: false,
             onLoadSuccess: function () {
                 //自动填入待修改考评项类型
-                if(checkItemData.length !== 0){
+                if (checkItemData.length !== 0) {
                     $('#checkItemTypeConfig').combobox('setValue', item.checkItemType);
                 }
             }
@@ -340,7 +430,7 @@ require(["jquery", 'util', "transfer", "easyui"], function ($, Util, Transfer) {
             editable: false,
             onLoadSuccess: function () {
                 //自动填入待修改考评项致命类别
-                if(vitalTypeData.length !== 0){
+                if (vitalTypeData.length !== 0) {
                     $('#checkItemVitalTypeConfig').combobox('setValue', item.checkItemVitalType);
                 }
             }
@@ -408,6 +498,7 @@ require(["jquery", 'util', "transfer", "easyui"], function ($, Util, Transfer) {
                 if (rspCode != null && rspCode === "1") {
                     $("#checkItemDialog").window("close");  //关闭对话框
                     $("#checkItemList").datagrid('reload'); //插入成功后，刷新页面
+                    refreshTree(); //刷新考评树
                 }
                 disableSubmit = false;
                 submitBtn.linkbutton({disabled: false});  //取消提交禁用
@@ -441,8 +532,46 @@ require(["jquery", 'util', "transfer", "easyui"], function ($, Util, Transfer) {
                     var rspCode = result.RSP.RSP_CODE;
                     if (rspCode != null && rspCode === "1") {
                         $("#checkItemList").datagrid('reload'); //删除成功后，刷新页面
+                        refreshTree(); //刷新考评树
                     }
                 });
+            }
+        });
+    }
+
+    //刷新考评树
+    function refreshTree() {
+        var zNodes = [];
+        var tenantId = $("#tenantType").combobox("getValue");
+        if (tenantId === "") {
+            tenantId = Util.constants.TENANT_ID;
+        }
+        var reqParams = {
+            "tenantId": tenantId
+        };
+        var params = $.extend({
+            "start": 0,
+            "pageNum": 0,
+            "params": JSON.stringify(reqParams)
+        }, Util.PageUtil.getParams($("#searchForm")));
+
+        Util.ajax.getJson(Util.constants.CONTEXT + Util.constants.CHECK_ITEM_DNS + "/queryCheckItem", params, function (result) {
+            var data = result.RSP.DATA;
+            checkItemListData = data;
+            for (var i = 0; i < data.length; i++) {
+                var nodeMap =
+                    {id: data[i].checkItemId, pId: data[i].parentCheckItemId, name: data[i].checkItemName};
+                zNodes.push(nodeMap);
+            }
+            $.fn.zTree.init($("#checkItemTree"), setting, zNodes);
+            if (data.length > 0) {
+                //父节点名称
+                checkNode.id = data[0].checkItemId;
+                checkNode.parentId = data[0].parentCheckItemId;
+                checkNode.name = data[0].checkItemName;
+                checkNode.level = 0;
+                checkNode.isParent = true;
+                $("#parentCheckItemName").val(checkNode.name)
             }
         });
     }
