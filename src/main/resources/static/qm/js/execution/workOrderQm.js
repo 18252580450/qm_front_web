f9189b81f3e743f5575ae4e80624e1876ea853a4
wangcheng <wangcheng@asiainfo.com>
2019-04-16 09:24:10 +0800

    考评评语修改带入信息BUG

--- src/main/resources/static/qm/js/execution/workOrderQm.js
+++ src/main/resources/static/qm/js/execution/workOrderQm.js
@@ -267,6 +267,14 @@ require(["js/manage/queryQmPlan","jquery", 'util', "transfer", "easyui","dateUti
                             return "<span title='" + value + "'>" + value + "</span>";
                         }
                 }},
+                {
+                    field: 'checkedTime', title: '抽取时间', align: 'center', width: '10%',
+                    formatter: function (value, row, index) { //格式化时间格式
+                        if (value) {
+                            return "<span title='" + DateUtil.formatDateTime(value) + "'>" + DateUtil.formatDateTime(value) + "</span>";
+                        }
+                    }
+                },
                 {field: 'crtTime', title: '提交时间', align: 'center', width: '10%',
                     formatter: function (value, row, index) { //格式化时间格式
                         if(value){
